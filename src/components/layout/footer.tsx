2c90084c506e2789c4d436ab34624770cd4792f3
hkv24 <harshkumarverma99@gmail.com>
2025-02-11 22:24:48 +0530

    final 2

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,9 +7,10 @@ const Footer = () => {
     <footer className="w-full bg-gray-50 py-6">
       <div className="flex items-center justify-center gap-1">
         <Typography className="flex items-center" variant="body3">
+          Designed and built with passion—
           <Copyright className="mr-1 inline-block h-4 w-4" />
-          {new Date().getFullYear()} |&nbsp;
-          &nbsp;by Harsh Kumar Verma
+          Harsh Kumar Verma,&nbsp;
+          {new Date().getFullYear()}
         </Typography>
       </div>
     </footer>
