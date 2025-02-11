17bd2fb5bf2c738aca0dee30f0a6139497ead059
hkv24 <harshkumarverma99@gmail.com>
2025-02-11 21:32:28 +0530

    fixes the broken link for download resume

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/harsh-resume.pdf', '_blank')}>
+    <Button onClick={() => window?.open('https://hkv24.github.io/portfolio-website/files/harsh-resume.pdf', '_blank')}>
       Download CV
     </Button>
   );
