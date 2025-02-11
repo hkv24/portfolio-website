87efa5a755657858ff4bcc10859b04e55c7be443
hkv24 <harshkumarverma99@gmail.com>
2025-02-11 22:44:12 +0530

    footer size fix

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-gray-50 py-6">
       <div className="flex items-center justify-center gap-1">
-        <Typography className="flex items-center" variant="body3">
+        <Typography className="flex items-center text-xs sm:text-base" variant="body3">
           Designed and built with passion—
           <Copyright className="mr-1 inline-block h-4 w-4" />
           Harsh Kumar Verma,&nbsp;
